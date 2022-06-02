ab5593ceb4c7d0e561aea65ca5dfeec3ac3f8a78
Prabhat Sharma <hi.prabhat@gmail.com>
2022-06-02 14:33:14 -0700

    exlude .git folder from s3 sync

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 
 # Move the files to S3 bucket for hosting
-aws s3 sync ./ s3://zincsearch-website/ --profile=zinc-prod
+aws s3 sync ./ s3://zincsearch-website/  --exclude=".git/*" --profile=zinc-prod
 
 # invalidate cloudfront cache so that latest files can be served
 aws cloudfront create-invalidation --distribution-id EHLGSI3HYP380 --paths=/* --profile=zinc-prod
